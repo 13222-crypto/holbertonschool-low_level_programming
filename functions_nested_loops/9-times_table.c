b8b80056c90b31c0783ad765ff3d0bc5677e6ec1
Bayadir Aldossari <13222@holbertonstudents.com>
2026-02-27 03:52:04 +0000

    add 9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: Nothing.
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int result;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			result = i * j;
+			if (j != 0)
+				_putchar(',');
+			if (j == 0)
+			{
+				_putchar('0' + result);
+			}
+			else if (result >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
