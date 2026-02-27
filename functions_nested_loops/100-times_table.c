8ebd0182d5015e3c1dd1e311a06835feff6662c2
Bayadir Aldossari <13222@holbertonstudents.com>
2026-02-27 04:11:29 +0000

    fix 100-times_table betty style

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,6 +1,40 @@
 #include "main.h"
 
 /**
+ * print_cell - prints a cell of the times table
+ * @result: the value to print
+ * @first: 1 if first column, 0 otherwise
+ */
+void print_cell(int result, int first)
+{
+	if (first)
+	{
+		_putchar('0' + result);
+		return;
+	}
+	_putchar(',');
+	_putchar(' ');
+	if (result >= 100)
+	{
+		_putchar('0' + result / 100);
+		_putchar('0' + (result / 10) % 10);
+		_putchar('0' + result % 10);
+	}
+	else if (result >= 10)
+	{
+		_putchar(' ');
+		_putchar('0' + result / 10);
+		_putchar('0' + result % 10);
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + result);
+	}
+}
+
+/**
  * print_times_table - prints the n times table
  * @n: the number
  *
@@ -10,7 +44,6 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
-	int result;
 
 	if (n < 0 || n > 15)
 		return;
@@ -20,35 +53,7 @@ void print_times_table(int n)
 		j = 0;
 		while (j <= n)
 		{
-			result = i * j;
-			if (j == 0)
-			{
-				_putchar('0' + result);
-			}
-			else if (result >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + result / 100);
-				_putchar('0' + (result / 10) % 10);
-				_putchar('0' + result % 10);
-			}
-			else if (result >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result / 10);
-				_putchar('0' + result % 10);
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result);
-			}
+			print_cell(i * j, j == 0);
 			j++;
 		}
 		_putchar('\n');
