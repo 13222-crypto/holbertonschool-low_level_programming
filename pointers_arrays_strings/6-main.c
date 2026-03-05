6acbb63d109dc6ecf59ef5bf3eb0982a49f5c619
Bayadir Aldossari <13222@holbertonstudents.com>
2026-03-05 21:57:19 +0000

    add task 6

--- pointers_arrays_strings/6-main.c
+++ pointers_arrays_strings/6-main.c
@@ -2,9 +2,7 @@
 
 int main(void)
 {
-	char *str;
-
-	str = "0123456789";
-	puts2(str);
+	puts2("0123456789");
+	puts2("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
 	return (0);
 }
