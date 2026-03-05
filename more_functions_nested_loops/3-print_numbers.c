41701e5c65bb3b9c4164071522d55afd3480de85
Bayadir Aldossari <13222@holbertonstudents.com>
2026-03-05 20:24:37 +0000

    add tsk 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,15 +7,9 @@
  */
 void print_numbers(void)
 {
-	_putchar('0');
-	_putchar('1');
-	_putchar('2');
-	_putchar('3');
-	_putchar('4');
-	_putchar('5');
-	_putchar('6');
-	_putchar('7');
-	_putchar('8');
-	_putchar('9');
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
