4e161cf5e081f172a86f1b806112f289fea4cdec
Bayadir Aldossari <13222@holbertonstudents.com>
2026-03-08 21:15:30 +0000

    add task 2

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,29 +1,21 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
- * main - test the _memcpy function
+ * main - test the _strchr function
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char buffer[98];
-	char buffer2[98];
-	char buffer3[98];
-	char buffer4[98];
+	char *s = "hello";
+	char *f;
 
-	strcpy(buffer, "This is a string");
-	strcpy(buffer2, "This is a string");
-	strcpy(buffer3, "This is a string");
-	strcpy(buffer4, "This is a string");
+	f = _strchr(s, 'l');
 
-	simple_print_buffer(buffer, strlen(buffer));
-	_memcpy(buffer + 5, buffer2, 8);
-	simple_print_buffer(buffer, strlen(buffer));
-	_memcpy(buffer3, buffer4, strlen(buffer4));
-	simple_print_buffer(buffer3, strlen(buffer4));
-	_memcpy(buffer4, "garbage can", 6);
-	simple_print_buffer(buffer4, strlen(buffer4));
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
 	return (0);
 }
