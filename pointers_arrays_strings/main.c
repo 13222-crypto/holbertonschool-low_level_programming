27a08cc9f57937a31b907f1c86946efbf10c40cc
Bayadir Aldossari <13222@holbertonstudents.com>
2026-03-09 02:50:36 +0000

    add task 7

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -2,23 +2,26 @@
 #include <stdio.h>
 
 /**
- * main - test the print_chessboard function
+ * main - test the print_diagsums function
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char board[8][8] = {
-		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'}
+	int c3[3][3] = {
+		{0, 1, 5},
+		{10, 11, 12},
+		{20, 21, 22}
+	};
+	int c5[5][5] = {
+		{0, 1, 2, 3, 4},
+		{5, 6, 7, 8, 9},
+		{10, 11, 12, 13, 14},
+		{15, 16, 17, 18, 19},
+		{20, 21, 22, 23, 24}
 	};
 
-	print_chessboard(board);
+	print_diagsums((int *)c3, 3);
+	print_diagsums((int *)c5, 5);
 	return (0);
 }
