384566706a712ebc2956fe94c773db25a6953fcc
Bayadir Aldossari <13222@holbertonstudents.com>
2026-03-09 02:52:26 +0000

    add task 8

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -2,26 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - test the print_diagsums function
+ * main - test the set_string function
  *
  * Return: Always 0
  */
 int main(void)
 {
-	int c3[3][3] = {
-		{0, 1, 5},
-		{10, 11, 12},
-		{20, 21, 22}
-	};
-	int c5[5][5] = {
-		{0, 1, 2, 3, 4},
-		{5, 6, 7, 8, 9},
-		{10, 11, 12, 13, 14},
-		{15, 16, 17, 18, 19},
-		{20, 21, 22, 23, 24}
-	};
+	char *s0 = "Bob Dylan";
+	char *s1 = "Robert Allen";
 
-	print_diagsums((int *)c3, 3);
-	print_diagsums((int *)c5, 5);
+	printf("%s, %s\n", s0, s1);
+	set_string(&s1, s0);
+	printf("%s, %s\n", s0, s1);
 	return (0);
 }
