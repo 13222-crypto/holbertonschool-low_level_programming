8f2339bea1a03eac90e7d4f185dbdc675c3c011a
Bayadir Aldossari <13222@holbertonstudents.com>
2026-03-14 18:17:34 +0000

    fix task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the program
+ * @ac: unused
+ * @argv: arguments vector
+ * Return: 0
+ */
 int main(int ac, char *argv[])
 {
-    (void)ac;
-    printf("%s\n", argv[0]);
-    return (0);
+	(void)ac;
+	printf("%s\n", argv[0]);
+	return (0);
 }
