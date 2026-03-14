e67506162691c3cc0f4cf2189e88a8629522478b
Bayadir Aldossari <13222@holbertonstudents.com>
2026-03-14 18:16:09 +0000

    fix task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
-/**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0
- */
-int main(int argc, char *argv[])
+int main(int ac, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return (0);
+    (void)ac;
+    printf("%s\n", argv[0]);
+    return (0);
 }
