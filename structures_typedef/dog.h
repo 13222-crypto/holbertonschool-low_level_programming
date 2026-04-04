ae7fa5ef1bb63d7654c5ec19e1499432c7ddc075
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-04 20:58:30 +0000

    Define dog_t as a new name for struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- */
+typedef struct dog dog_t;
+
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
-void print_dog(struct dog *d);
-
-#endif /* DOG_H */
+#endif
