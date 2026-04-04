78c85a7dff296fbd88ce3420b9dccdfe2e9617eb
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-04 20:56:18 +0000

    Add init_dog function and update header

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,17 +2,19 @@
 #include "dog.h"
 
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: pointer to the dog struct
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to set
+ * @age: age to set
+ * @owner: owner to set
+ *
+ * Return: nothing
  */
-void init_dog(struct dog *d, char *name,
-	float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
