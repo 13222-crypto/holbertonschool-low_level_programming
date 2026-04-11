95019f2710e8c63bb31aef5361f9309e61798594
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-11 11:10:56 +0000

    fix exit codes task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -19,14 +19,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 	if (f == NULL)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	printf("%d\n", f(num1, num2));
 	return (0);
 }
