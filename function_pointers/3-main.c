c7c79758e4451e305191001bc898ab105b2756fa
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-11 11:05:40 +0000

    fix task 3 structure

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-op_functions.h"
+
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 98 on error.
+ */
+int main(int argc, char *argv[])
+{
+	int (*f)(int, int);
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	printf("%d\n", f(num1, num2));
+	return (0);
+}
