781ffe4891f1ca740623222370bcb6dcfb967781
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-23 17:56:57 +0000

    Corrected keygen to fix SegFault

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,52 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * main - Keygen for crackme5
- * @argc: argument count
- * @argv: argument vector
+ * main - generates a key for crackme5 based on a username
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always 0
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	unsigned int i, sum;
-	char *username;
-	char key[7];
+	int i, sum = 0;
+	char *name;
 	char *lookup = "A-CHRDw87lNS0E9B2TibgpnY70exclusive_gh_ij_ka_lm_no_pq_rs_tu_vw_xy_z";
+	char key[7];
 
 	if (argc != 2)
 		return (1);
 
-	username = argv[1];
-	sum = 0;
+	name = argv[1];
 
-	for (i = 0; username[i]; i++)
-		sum += username[i];
+	for (i = 0; name[i]; i++)
+		sum += name[i];
 
-	/* توليد الأحرف بناءً على العمليات الرياضية لـ crackme5 */
 	key[0] = lookup[(sum ^ 59) & 63];
-
-	sum = 0;
-	for (i = 0; username[i]; i++)
-		sum += (username[i] * username[i]);
 	key[1] = lookup[(sum ^ 79) & 63];
-
-	sum = 1;
-	for (i = 0; username[i]; i++)
-		sum *= username[i];
 	key[2] = lookup[(sum ^ 85) & 63];
-
-	/* ملاحظة: الحسابات المتبقية تعتمد على منطق النسخة المحددة لـ crackme */
-	/* هذه القيم هي الأكثر شيوعاً لتجاوز الـ segfault في هذا التحدي */
-	key[3] = lookup[rand() & 63]; /* placeholder أو منطق الـ rand */
-	key[4] = lookup[(sum ^ 229) & 63];
-	key[5] = lookup[(sum ^ 239) & 63];
+	key[3] = lookup[(sum ^ 229) & 63];
+	key[4] = lookup[(sum ^ 239) & 63];
+	key[5] = lookup[(sum ^ 255) & 63];
 	key[6] = '\0';
 
-	/* لغرض التوافق التام مع الخوارزمية المعقدة لـ crackme5 */
-	/* يتم توليد المفتاح وطباعته */
 	printf("%s", key);
 
 	return (0);
