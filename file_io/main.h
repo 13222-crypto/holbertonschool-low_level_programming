a7b4ef0b279a27e7e84c9ca143374285bac9367c
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-25 20:43:06 +0000

    Update main.h and add task 1

--- file_io/main.h
+++ file_io/main.h
@@ -3,8 +3,11 @@
 
 #include <stddef.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
