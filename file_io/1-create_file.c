f68ec91834138cfeb71c69e831da2f6c793d6e8b
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-25 20:46:14 +0000

    Fix: final Betty line length adjustment

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,6 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	/* قمنا بتقسيم هذا السطر ليكون أقل من 80 حرفاً */
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
