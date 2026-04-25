4df5bbe46ec6f78337fac542d23408b55fc67575
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-25 20:54:55 +0000

    Fix: Correct OS/ABI mapping for Solaris

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -95,20 +95,21 @@ void print_version(unsigned char *e_ident)
  */
 void print_osabi(unsigned char *e_ident)
 {
-	const char *osabis[] = {
-		"UNIX - System V", "UNIX - HP-UX", "UNIX - NetBSD",
-		"UNIX - Linux", "UNIX - GNU Hurd", "UNIX - Solaris",
-		"UNIX - IRIX", "UNIX - FreeBSD", "UNIX - TRU64",
-		"Novell - NetWare", "UNIX - OpenBSD", "UNIX - OpenVMS",
-		"NonStop - Kernel", "AROS", "FenixOS", "CloudABI"
-	};
 	printf("  OS/ABI:                            ");
-	if (e_ident[EI_OSABI] < 16)
-		printf("%s\n", osabis[e_ident[EI_OSABI]]);
-	else if (e_ident[EI_OSABI] == ELFOSABI_STANDALONE)
-		printf("Standalone App\n");
-	else
-		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
+	switch (e_ident[EI_OSABI])
+	{
+	case ELFOSABI_NONE: printf("UNIX - System V\n"); break;
+	case ELFOSABI_HPUX: printf("UNIX - HP-UX\n"); break;
+	case ELFOSABI_NETBSD: printf("UNIX - NetBSD\n"); break;
+	case ELFOSABI_LINUX: printf("UNIX - Linux\n"); break;
+	case ELFOSABI_SOLARIS: printf("UNIX - Solaris\n"); break;
+	case ELFOSABI_IRIX: printf("UNIX - IRIX\n"); break;
+	case ELFOSABI_FREEBSD: printf("UNIX - FreeBSD\n"); break;
+	case ELFOSABI_TRU64: printf("UNIX - TRU64\n"); break;
+	case ELFOSABI_ARM: printf("ARM\n"); break;
+	case ELFOSABI_STANDALONE: printf("Standalone App\n"); break;
+	default: printf("<unknown: %x>\n", e_ident[EI_OSABI]); break;
+	}
 }
 
 /**
