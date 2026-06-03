bc6acd62ad3c036d7752c63eff786588602480a0
Bayadir Aldossari <13222@holbertonstudents.com>
2026-06-03 20:18:58 +0000

    Fix memory boundaries and prevent heap corruption in 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -64,41 +64,49 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, j, carry, digit1, digit2, *result, a = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 		errors();
+
+	s1 = argv[1];
+	s2 = argv[2];
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2;
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len; i++)
+
+	for (i = 0; i < len; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = s1[i] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (j = len2 - 1; j >= 0; j--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			digit2 = s2[j] - '0';
+			carry += result[i + j + 1] + (digit1 * digit2);
+			result[i + j + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			result[i + j + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
+
+	for (i = 0; i < len; i++)
 	{
 		if (result[i])
 			a = 1;
 		if (a)
 			_putchar(result[i] + '0');
 	}
-	_putchar(result[i] + '0');
+
+	if (!a)
+		_putchar('0');
+
 	_putchar('\n');
 	free(result);
 	return (0);
